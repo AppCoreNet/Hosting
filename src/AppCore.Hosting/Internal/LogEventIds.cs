093f07f0460ddd0b9017e1a157856169211d6310
Christian Prochnow <christian.prochnow77@gmail.com>
2019-08-31 20:18:02 +0200

    Improved log message.

--- src/AppCore.Hosting/Internal/LogEventIds.cs
+++ src/AppCore.Hosting/Internal/LogEventIds.cs
@@ -10,10 +10,10 @@ namespace AppCore.Hosting
 
         public static readonly LogEventId ServiceStartFailed = new LogEventId(2, nameof(ServiceStartFailed));
 
-        public static readonly LogEventId ServiceStopping = new LogEventId(0, nameof(ServiceStopping));
+        public static readonly LogEventId ServiceStopping = new LogEventId(3, nameof(ServiceStopping));
 
-        public static readonly LogEventId ServiceStopped = new LogEventId(1, nameof(ServiceStopped));
+        public static readonly LogEventId ServiceStopped = new LogEventId(4, nameof(ServiceStopped));
 
-        public static readonly LogEventId ServiceStopFailed = new LogEventId(2, nameof(ServiceStopFailed));
+        public static readonly LogEventId ServiceStopFailed = new LogEventId(5, nameof(ServiceStopFailed));
     }
 }
